146c77fae6ef534c21efff0549bff7e31fc4e883
Danny Barker <barkerdanny17@yahoo.com>
2019-06-05 13:11:01 -0500

    journal entry day 2

---
+++ src/scripts/scripts.js
@@ -0,0 +1,10 @@
+let journalEntries = [];
+
+const journalEntry = {
+    date: '6/05/19',
+    concepts: `Javascript`,
+    long_form: `We learned javascript functions`,
+    mood: `alright`,
+}
+
+journalEntries.push(journalEntry);

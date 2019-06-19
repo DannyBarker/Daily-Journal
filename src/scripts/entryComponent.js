1438921c6f3afc1188643e65728ca70cc4780fe3
Danny Barker <barkerdanny17@yahoo.com>
2019-06-18 22:19:40 -0500

    got stpe 2 to work

--- src/scripts/entryComponent.js
+++ src/scripts/entryComponent.js
@@ -7,9 +7,20 @@ const addToDB = () => {
         let long_formInput = document.querySelector("#journalEntry")
         let moodInput = document.querySelector("#mood")
         if (dateInput.value && conceptInput.value && long_formInput.value && moodInput.value) {
-            document.querySelector("#go-here").innerHTML = ''
-            let newObj = makeJournalObj()
-           API.addJournalEntry(newObj).then( data => API.getJournalEntries().then(renderJournalEntries))
+               let conceptCharacters = conceptInput.value
+               let long_formCharacters = long_formInput.value
+                let x = /[^a-zA-Z0-9(){}:;\s]/
+                let conceptContain = conceptCharacters.match(x)
+                let long_formContain = long_formCharacters.match(x)
+
+                if (conceptContain === null && long_formContain === null) {
+                        document.querySelector("#go-here").innerHTML = ''
+                        let newObj = makeJournalObj()
+                        API.addJournalEntry(newObj).then( data => API.getJournalEntries().then(renderJournalEntries))
+
+                }
+               
+                
         }
         dateInput.value = ""
         conceptInput.value = ""
